4d833115193634b4572dbb34a92ba9e7b495e0c5
JSaturn9 <JSaturn9@github.com>
2020-11-02 17:58:09 -0500

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     Weather App 
+     <footer>
+       This project was coded by Joanna Saturn and is open-sourced on {""}
+     <a href="https://github.com/JSaturn9/shecodes-react-final-project" target= "_blank">GitHub</a> 
+     </footer>
+     
     </div>
   );
 }
