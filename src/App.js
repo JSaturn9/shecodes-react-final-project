8a3f666ed8aa1891ad758418f6a24daf5a308971
JSaturn9 <JSaturn9@github.com>
2020-11-05 15:25:27 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App 
+      <div className="container">
+    <h1>Weather App </h1> 
+    <button className="btn btn-primary">Here</button>
      <footer>
        This project was coded by Joanna Saturn and is open-sourced on {""}
      <a href="https://github.com/JSaturn9/shecodes-react-final-project" target= "_blank">GitHub</a> 
      </footer>
-     
+     </div>
     </div>
   );
 }
