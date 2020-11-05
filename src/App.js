bfd6132d52750fab94c7834abc6bc4fc4a556c3f
JSaturn9 <JSaturn9@github.com>
2020-11-05 15:47:56 -0500

    Created Weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-    <h1>Weather App </h1> 
-    <button className="btn btn-primary">Here</button>
+    
+    <Weather />
+    
      <footer>
        This project was coded by Joanna Saturn and is open-sourced on {""}
      <a href="https://github.com/JSaturn9/shecodes-react-final-project" target= "_blank">GitHub</a> 
